5ee02f6b07d52dbca626dec0be5553d721a05798
deepakguupt83 <deepakgupta83404@gmail.com>
2026-04-15 18:31:00 +0530

    front

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "https://ai-recommendation-kk1u.vercel.app",
       methods: ["GET", "POST"],
       allowedHeaders: ["Content-Type"]
     })
